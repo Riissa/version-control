87f4374958f03198b4901ddf0fc4913557140239
Riissa <rpiland@uci.edu>
2025-01-29 08:06:33 +0000

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 #This gets the current date and time
 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-#This wWrite to the file version.md
+#This writes to the file version.md
 with open("version.md", "w") as file:
     file.write(f"Last updated: {current_time}\n")
 
